7141582d488445b6090cf1cf97a6dd02a62fc1c2
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-02-16 15:33:24 +0530

    data for products:

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const  data = [
+  {
+    id : "848484801",
+    title : "Amazon Basics Nylon Braided Lightning to USB A Cable, MFi Certified Apple iPhone Charger, Dark Gray, 6-Foot ",
+    price : 119.6,
+    rating : 5,
+    image : require("./images/1_Data_Cable.jpg").default
+  },
+  {
+    id : "848484802",
+    title : "Amazon Basics Home Keypad Safe - 1.2 Cubic Feet, 13 x 13 x 14.2 Inches, Black - 36SAM  ",
+    price : 1400,
+    rating : 4,
+    image : require("./images/2_Box.jpg").default
+  },
+  {
+    id : "848484803",
+    title : "Amazon Basics 36 Pack AAA High-Performance Alkaline Batteries, 10-Year Shelf Life, Easy to Open Value Pack  ",
+    price : 600,
+    rating : 3,
+    image : require("./images/3_Battery.jpg").default
+  },
+  {
+    id : "848484804",
+    title : "AmazonBasics Legal/Wide Ruled 8-1/2 by 11-3/4 Legal Pad - White (50 Sheet Paper Pads, 12 pack)",
+    price : 110.96,
+    rating : 5,
+    image : require("./images/4_Paper.jpg").default
+  },
+  {
+    id : "848484805",
+    title : "Toshiba Canvio Flex 4TB Portable External Hard Drive USB-C USB 3.0, Silver for PC, Mac, & Tablet - HDTX140XSCCA ",
+    price : 6000,
+    rating : 2,
+    image : require("./images/5_HardDisk.jpg").default
+  },
+  {
+    id : "848484806",
+    title : "Apricorn 1TB Aegis Padlock USB 3.0 256-bit AES XTS Hardware Encrypted Portable External Hard Drive (A25-3PL256-1000)  ",
+    price : 12000,
+    rating : 5,
+    image : require("./images/6_Hard_Disk.jpg").default
+  },
+  {
+    id : "848484807",
+    title : "Apricorn Aegis Secure Key 3 NX 32GB 256-Bit Encrypted FIPS 140-2 Level 3 Validated Secure USB 3.0 Flash Drive, ASK3-NX-32GB, Black ",
+    price : 4000,
+    rating : 4,
+    image : require("./images/7_Pen_Drive.jpg").default
+  },
+  {
+    id : "848484808",
+    title : "USB Flash Drive 128GB, USB 3.1 Flash Drive 400MB/s, VANSUNY Super Speed Solid State USB Drive Thumb Drive Slide Metal USB Memory Stick Zip Drive Jump Drive Pendrive (128G,Black) ",
+    price : 1500,
+    rating : 2,
+    image : require("./images/8_Pen_Drive.jpg").default
+  },
+  {
+    id : "848484809",
+    title : "HP 63 | Ink Cartridge | Black | F6U62AN ",
+    price : 1196,
+    rating : 2,
+    image : require("./images/9_Ink.jpg").default
+  },
+  {
+    id : "848484810",
+    title : "VANSUNY Flash Drive 128GB USB C USB Flash Drive Dual Type C USB 3.0 OTG with Keychain for Laptop PC PDA Smartphones Tablet Samsung Black",
+    price : 1900,
+    rating : 4,
+    image : require("./images/10_Pen_Drive.jpg").default
+  },
+]

b30f00b3de23af7e2e52f8b26bf9afb2f33c66f7
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-02-16 22:04:22 +0530

    adding functionlity for bucket and checkout

---
+++ src/CheckOut.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import useStateValue from './DataLayer';
+import CheckoutProduct from './CheckoutProduct';
+
+function CheckOut() {
+
+  const [{basket}] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <img className="checkout_ad"
+         src={require("./discountBanner.jpg").default}
+    />
+    {basket?.length === 0 ? (
+      <div>
+        <h2> Your Shopping Bucket is empty</h2>
+      </div>
+    ) : (
+      <div>
+        <h2> Your Shopping Bucket</h2>
+        {/* List all the all checkout item */}
+        {basket?.map(item => (
+          <CheckoutProduct
+            id={item.id}
+             title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              quantity={item.quantity}
+            />
+        ))}
+      </div>
+    )}
+    <h1>Beniwal</h1>
+    </div>
+  )
+}
+
+export default CheckOut
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './checkoutproduct.css'
+import useStateValue from './DataLayer';
+
+function CheckoutProduct({id , title , image , price , rating, quantity}) {
+
+  const [{basket}, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch(
+      {
+        type : "REMOVE_FROM_BASKET" ,
+        id : id
+      }
+    )
+  }
+  return (
+    <div className="checkoutProduct">
+      <div className="checkoutproduct_image">
+        <img src={image} className="checkoutproduct_image" alt=""/>
+          </div>
+          <div className="checkoutproduct_info">
+        <p className="checkoutproduct_title">{title}</p>
+        <p className="checkoutproduct_price">
+          <small>&#8377;</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutproduct_rating">
+          {
+            Array(rating)
+            .fill()
+            .map((_ , index )=> <p key={index}>⭐</p>)
+          }
+        </div>
+        <div>
+        <span>Quantity : </span><span>{quantity}</span>
+      </div>
+      <button onClick={removeFromBasket} >Remove from Basket</button>
+
+    </div>
+  </div>
+  )
+}
+
+export default CheckoutProduct
